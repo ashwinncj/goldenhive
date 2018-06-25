10e497232b404fe60f94990f2a3ced7c591ce790
ashwinncj <30401526+ashwinncj@users.noreply.github.com>
2018-06-25 23:48:59 +0530

    Updated the mine-button

--- plugin/mine-button.php
+++ plugin/mine-button.php
@@ -7,4 +7,11 @@ wp_register_script('angular.min', plugins_url('../assets/angular.min.js', __FILE
 wp_enqueue_script('angular.min');
 wp_register_style('shape-main', plugins_url('css/main.css', __FILE__));
 wp_enqueue_style('shape-main');
+wp_register_script('golden-widget', plugins_url('lib/widget.js', __FILE__));
+wp_enqueue_script('golden-widget');
+wp_localize_script('golden-widget', 'widget', array(
+    'pluginsUrl' => plugins_url('/', __FILE__),
+    'color' => 'golden',
+    'param' => 'hive'
+));
 ?>
