7d7501de0326fd82c422b1caadfc5466a15c5730
Ashwin Kumar C <ashwinncj@gmail.com>
2018-06-25 23:35:08 +0530

    Create mine-button.php

---
+++ plugin/mine-button.php
@@ -0,0 +1,8 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">        
+
+<?php
+wp_register_script('angular.min', plugins_url('../assets/angular.min.js', __FILE__));
+wp_enqueue_script('angular.min');
+wp_register_style('shape-main', plugins_url('css/main.css', __FILE__));
+wp_enqueue_style('shape-main');
+?>
